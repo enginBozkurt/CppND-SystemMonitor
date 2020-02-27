847d9f541e303b44c8073c6a0fbc157ac9fa86be
Joao Marques <jpmarques19@gmail.com>
2020-02-27 13:06:16 +0000

    Implement time formatting function.

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,16 @@
 #include <string>
-
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    int hour = seconds / 3600;
+    short minute = (seconds % 3600) / 60;
+    short second = (seconds % 3600) % 60;
+    char buffer[8];
+    sprintf(buffer,"%.2d:%.2d:%.2d",hour,minute,second);
+    string elapsed_time(buffer);
+    return elapsed_time;
+}
